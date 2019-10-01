9a9652f5453237b913a41c6875936a7e690db316
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2019-10-01 14:22:41 +0900

    Create 5week_LED1.c

---
+++ C/5week_LED1.c
@@ -0,0 +1,31 @@
+//LED 0번부터 8번까지 순서대로 켜고 끄기
+
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 16000000
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "_main.h"
+
+void PB_Lshift(){
+	char LED;
+	int i;
+	LED=0xfe;
+	for (i=0;i<8;i++){
+		_delay_ms(500);
+		PORTB =LED;
+		LED=(LED<<1)|0x01;
+	}
+}
+
+int main(void)
+{
+    DDRB =0xff;
+    while (1) 
+    {
+		PB_Lshift();
+		_delay_ms(500);
+		   }
+}
+
