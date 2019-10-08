985a85d50dd86715d793ed1484b31590aae63973
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2019-10-08 14:29:07 +0900

    Create 6week_Interrut_toggle_switch.c

---
+++ C/6week_Interrut_toggle_switch.c
@@ -0,0 +1,40 @@
+//인터럽트 '토글'을 이용하여 버튼을 누르면 LED가 켜지는 프로그램
+//0번 스위치를 한번 누를 때마다 LED는 2진수로 켜진다.
+//ex) 
+// 누르기전 - 00000000  
+// 1번 누른 뒤 - 00000001
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "_main.h"
+#define F_CPU 14745600UL
+
+char exchange;
+
+SIGNAL(INT0_vect) {
+	exchange = ~exchange;
+}
+
+void Interrupt_init(void){
+	SREG |=0x88;
+	EICRA = 0x00;  //INT 0의 Falling Edge trigger
+	EIMSK = 0x01;  //INT 0만 허가
+	DDRD=0x00;  //INT0~3은 PD0~3
+} 
+
+int main(void)
+{
+		Interrupt_init();
+		DDRB=0xff;
+		PORTB =0x00;
+		exchange =0;
+		while(1){
+			
+			if(exchange) PORTB=0xff;
+			else PORTB=0x00;
+		}
+}
+
