be85f579995c0fd488d6f8c62cc2ea8f231837e7
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-01-06 23:49:57 +0900

    Create 11-1. cin으로 입력받기.cpp

---
+++ 11-1. cin으로 입력받기.cpp
@@ -0,0 +1,16 @@
+//cin은 공백 이후를 입력받지 않는다
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string msg;
+	
+	cout << "과목 입력: ";
+
+	cin >> msg;							
+
+	cout << "출력: " << msg << endl;
+  
+}
