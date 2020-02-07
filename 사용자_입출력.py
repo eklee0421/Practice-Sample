35e1a6f0f5f48f7029cceb7dbd0736eb0eaba777
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2020-02-07 17:02:13 +0900

    Create 사용자_입출력.py

---
+++ 사용자_입출력.py
@@ -0,0 +1,8 @@
+number = input("숫자를 입력하세요: ") #프롬프트로부터 사용자에게 숫자를 입력 받는다
+print(number) #입력 받은 숫자를 출력한다
+
+for i in range(10):
+  print(i, end=' ') #한칸씩 띄워서 출력 0 1 2 3 4 5 6 7 8 9
+
+for i in range(10):
+  print(i)  #이렇게 하면 endl
