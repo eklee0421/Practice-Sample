cdff71b5bb29f71061d915fe8e7b0133fb6a6d7d
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2021-06-18 15:41:49 +0900

    create wpfPreset

---
+++ C#/WPF/WpfPreset/WpfPreset/Model/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPreset.Model
+{
+    public class Product
+    {
+        public Product(string NumCode, string Name, int Price)
+        {
+            this.NumCode = NumCode;
+            this.Name = Name;
+            this.Price = Price;
+        }
+
+        public string NumCode { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+    }
+
+    
+}
---
+++ C#/WPF/WpfPreset/WpfPreset/ViewModel/InputNumCodeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPreset.ViewModel
+{
+    class InputNumCodeViewModel : ViewModelBase
+    {
+        private string inputString = String.Empty;
+        public string InputString
+        {
+            set
+            {
+                inputString = value;
+                OnPropertyChanged("InputString");
+            }
+            get
+            {
+                return inputString;
+            }
+        }
+    }
+}
