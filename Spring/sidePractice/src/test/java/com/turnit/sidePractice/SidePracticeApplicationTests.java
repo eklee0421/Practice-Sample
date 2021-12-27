d34c3968f362cbb94372ba83f35815a1864d7829
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2021-12-27 17:43:44 +0900

    spring practice 프로젝트 생성

---
+++ Spring/sidePractice/src/test/java/com/turnit/sidePractice/SidePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnit.sidePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
