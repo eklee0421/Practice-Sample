7212b645d2af5b0b3927036d6206ca82446b04f8
eklee0421 <52737339+eklee0421@users.noreply.github.com>
2022-02-04 13:56:03 +0900

    회원 도메인과 레포지토리 예제

---
+++ Spring/sidePractice/src/main/java/com/turnit/sidePractice/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.turnit.sidePractice.repository;
+
+import com.turnit.sidePractice.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ Spring/sidePractice/src/main/java/com/turnit/sidePractice/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package com.turnit.sidePractice.repository;
+
+import com.turnit.sidePractice.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));  //Optional.ofNullable() - null 이어도 된다.
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        //람다식
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
